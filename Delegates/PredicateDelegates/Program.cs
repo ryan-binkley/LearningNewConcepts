da5e82a56da9d336d0bd45c2390ae8e0e1170673
Coolsnake8 <foxtailrb@gmail.com>
2022-11-03 23:45:57 -0600

    Created basic predicate delegate for 'IsEven' method.

--- Delegates/PredicateDelegates/Program.cs
+++ Delegates/PredicateDelegates/Program.cs
@@ -6,7 +6,19 @@ namespace PredicateDelegates
     {
         static void Main(string[] args)
         {
-            
+            Predicate<int> predicateDelegate = IsEven;
+
+            Console.WriteLine(predicateDelegate(4));
+            Console.WriteLine(predicateDelegate(7));
+            Console.WriteLine(predicateDelegate(2));
+        }
+        
+        static bool IsEven(int inputNum)
+        {
+            if ((inputNum % 2) == 0)
+                return true;
+            else
+                return false;
         }
     }
 }
