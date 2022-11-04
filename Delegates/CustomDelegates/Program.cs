b68bfbab8d92a809e0891c5f6f76cbd7ab69e1dd
Coolsnake8 <foxtailrb@gmail.com>
2022-11-03 23:26:57 -0600

    Added basic delegate code.

--- Delegates/CustomDelegates/Program.cs
+++ Delegates/CustomDelegates/Program.cs
@@ -4,9 +4,24 @@ namespace CustomDelegates
 {
     class Program
     {
+        public delegate int ReturnResult(int x, int y, int z);
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            ReturnResult calculator = new ReturnResult(Add);
+            calculator += Multiply;
+
+            Console.WriteLine(calculator.Invoke(17, 4, 20));
+        }
+
+        public static int Add(int num1, int num2, int num3)
+        {
+            return num1 + num2 + num3;
+        }
+
+        public static int Multiply(int num1, int num2, int num3)
+        {
+            return num1 * num2 * num3;
         }
     }
 }
